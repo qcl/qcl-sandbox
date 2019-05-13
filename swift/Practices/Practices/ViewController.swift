acd531c18cce4541ab5990f03fb4995280b174d6
Qing-Cheng Li <qc.linux@gmail.com>
2019-05-13 13:56:20 +0200

    Using xcode project to do the practice instead of using playground

---
+++ swift/Practices/Practices/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Practices
+//
+//  Created by Qingcheng Li on 5/13/19.
+//  Copyright © 2019 Qing-Cheng Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View did appear!")
+    }
+}
+
